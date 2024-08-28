eb29aec53eea0380ba04e7a7374bb0d255440b7c
RichardHoa <richardhoa.work@gmail.com>
2024-08-28 21:43:42 +0700

    separate functions

--- pokemon.go
+++ pokemon.go
@@ -3,11 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"strings"
 
-	"github.com/tidwall/gjson"
 )
 
 type PokemonLocation struct {
@@ -23,23 +19,7 @@ type LocationResult struct {
 	URL  string `json:"url"`
 }
 
-func FetchLocationData(URL string) []byte {
-	resp, err := http.Get(URL)
-	if err != nil {
-		fmt.Println("Error fetching data:", err)
-		return nil
-	}
-	defer resp.Body.Close()
 
-	data, err := io.ReadAll(resp.Body)
-	// fmt.Printf("data: %s\n", data)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		return nil
-	}
-
-	return data
-}
 
 func HandleMapCommand(location *PokemonLocation, cache *Cache) {
 	var URL string
@@ -125,25 +105,19 @@ func HandleMapbCommand(location *PokemonLocation, cache *Cache) {
 	location.Page--
 }
 
-func ParseInput(input string) (string, []string) {
-	parts := strings.Fields(input)
-	if len(parts) == 0 {
-		return "", nil
-	}
-	return parts[0], parts[1:]
-}
+
 
 // HandleExploreCommand handles the "explore" command with a city argument
 func HandleExploreCommand(city string, cache *Cache) {
 	URL := "https://pokeapi.co/api/v2/location/" + city
 
-	areaUrl, err := fetchAndExtractAreaURL(URL, cache)
+	areaUrl, err := FetchAndExtractAreaURL(URL, cache)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
-	pokemonList, err := fetchPokemonNames(areaUrl, cache)
+	pokemonList, err := FetchPokemonNames(areaUrl, cache)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
@@ -155,79 +129,3 @@ func HandleExploreCommand(city string, cache *Cache) {
 	}
 }
 
-func fetchAndExtractAreaURL(url string, cache *Cache) (areaURL string, err error) {
-	var body []byte
-
-	// Check if data is in cache
-	body, dataExist := cache.Get(url)
-	if !dataExist {
-		fmt.Printf("URL has never been cached: %s\n", url)
-		// Data is not cached, fetch it from the URL
-		resp, err := http.Get(url)
-		if err != nil {
-			return "", fmt.Errorf("failed to fetch URL: %v", err)
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			return "", fmt.Errorf("unexpected status code: %d | Possibly wrong location name", resp.StatusCode)
-		}
-
-		body, err = io.ReadAll(resp.Body)
-		if err != nil {
-			return "", fmt.Errorf("failed to read response body: %v", err)
-		}
-
-		// Add the newly fetched data to the cache
-		cache.Add(url, body)
-	} else {
-		fmt.Printf("Url has been cached %s\n", url)
-	}
-
-	// Extract area URL from the body
-	result := gjson.GetBytes(body, "areas.0.url")
-	if !result.Exists() {
-		return "", fmt.Errorf("area URL not found in JSON")
-	}
-
-	return result.String(), nil
-}
-
-func fetchPokemonNames(url string, cache *Cache) ([]string, error) {
-	var body []byte
-
-	// Check if data is in cache
-	body, dataExist := cache.Get(url)
-
-	if !dataExist {
-		fmt.Printf("URL has never been cached: %s\n", url)
-		// Data is not cached, fetch it from the URL
-		resp, err := http.Get(url)
-		if err != nil {
-			return []string{}, fmt.Errorf("failed to fetch URL: %v", err)
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			return []string{}, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
-		}
-
-		body, err = io.ReadAll(resp.Body)
-		if err != nil {
-			return []string{}, fmt.Errorf("failed to read response body: %v", err)
-		}
-		// Add the newly fetched data to the cache
-		cache.Add(url, body)
-	} else {
-		fmt.Printf("Url has been cached %s\n", url)
-	}
-
-	var pokemonNames []string
-	result := gjson.GetBytes(body, "pokemon_encounters.#.pokemon.name")
-	result.ForEach(func(_, value gjson.Result) bool {
-		pokemonNames = append(pokemonNames, value.String())
-		return true
-	})
-
-	return pokemonNames, nil
-}
---
+++ supportFunctions.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"github.com/tidwall/gjson"
+)
+
+
+
+func FetchLocationData(URL string) []byte {
+	resp, err := http.Get(URL)
+	if err != nil {
+		fmt.Println("Error fetching data:", err)
+		return nil
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	// fmt.Printf("data: %s\n", data)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return nil
+	}
+
+	return data
+}
+
+
+
+func ParseInput(input string) (string, []string) {
+	parts := strings.Fields(input)
+	if len(parts) == 0 {
+		return "", nil
+	}
+	return parts[0], parts[1:]
+}
+
+
+func FetchAndExtractAreaURL(url string, cache *Cache) (areaURL string, err error) {
+	var body []byte
+
+	// Check if data is in cache
+	body, dataExist := cache.Get(url)
+	if !dataExist {
+		fmt.Printf("URL has never been cached: %s\n", url)
+		// Data is not cached, fetch it from the URL
+		resp, err := http.Get(url)
+		if err != nil {
+			return "", fmt.Errorf("failed to fetch URL: %v", err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return "", fmt.Errorf("unexpected status code: %d | Possibly wrong location name", resp.StatusCode)
+		}
+
+		body, err = io.ReadAll(resp.Body)
+		if err != nil {
+			return "", fmt.Errorf("failed to read response body: %v", err)
+		}
+
+		// Add the newly fetched data to the cache
+		cache.Add(url, body)
+	} else {
+		fmt.Printf("Url has been cached %s\n", url)
+	}
+
+	// Extract area URL from the body
+	result := gjson.GetBytes(body, "areas.0.url")
+	if !result.Exists() {
+		return "", fmt.Errorf("area URL not found in JSON")
+	}
+
+	return result.String(), nil
+}
+
+func FetchPokemonNames(url string, cache *Cache) ([]string, error) {
+	var body []byte
+
+	// Check if data is in cache
+	body, dataExist := cache.Get(url)
+
+	if !dataExist {
+		fmt.Printf("URL has never been cached: %s\n", url)
+		// Data is not cached, fetch it from the URL
+		resp, err := http.Get(url)
+		if err != nil {
+			return []string{}, fmt.Errorf("failed to fetch URL: %v", err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return []string{}, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		}
+
+		body, err = io.ReadAll(resp.Body)
+		if err != nil {
+			return []string{}, fmt.Errorf("failed to read response body: %v", err)
+		}
+		// Add the newly fetched data to the cache
+		cache.Add(url, body)
+	} else {
+		fmt.Printf("Url has been cached %s\n", url)
+	}
+
+	var pokemonNames []string
+	result := gjson.GetBytes(body, "pokemon_encounters.#.pokemon.name")
+	result.ForEach(func(_, value gjson.Result) bool {
+		pokemonNames = append(pokemonNames, value.String())
+		return true
+	})
+
+	return pokemonNames, nil
+}
